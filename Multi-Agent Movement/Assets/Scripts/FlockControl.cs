591fdb80496d1f7269009be33d4d524f00b70094
CJ Legato <legatc@rpi.edu>
2018-10-09 17:35:42 -0400

    Some working flocking stuff, still needs a bit more

---
+++ Multi-Agent Movement/Assets/Scripts/FlockControl.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockControl : MonoBehaviour {
+
+    public GameObject boid;
+    [Range(5, 50)]
+    public int numBoids;
+
+    Camera cam;
+
+    [HideInInspector]
+    static public Vector2 center;
+    [HideInInspector]
+    static public Vector2 target;
+
+
+    Vector2 mousePos;
+
+    GameObject[] boids;
+
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;
+
+        boids = new GameObject[numBoids];
+        for (int i = 0; i < numBoids; i++)
+        {
+            float x, y;
+            x = Random.Range(numBoids * -0.05f, numBoids * 0.05f);
+            y = Random.Range(numBoids * -0.05f, numBoids * 0.05f);
+            GameObject temp = Instantiate(boid, new Vector3(x, y), Quaternion.identity);
+            boids[i] = temp;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector2 sum = Vector2.zero;
+		foreach (GameObject boid in boids)
+        {
+            sum += (Vector2)boid.transform.position;
+        }
+        center = sum / numBoids;
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        target = mousePos - center;
+
+	}
+}
--- Multi-Agent Movement/Assets/Scripts/Flocking.cs
+++ Multi-Agent Movement/Assets/Scripts/Flocking.cs
@@ -4,13 +4,76 @@ using UnityEngine;
 
 public class Flocking : MonoBehaviour {
 
+    public float maxVelocity;
+    public float maxAcceleration;
+
+    public float cohesionForce;
+    public float separationForce;
+    public float alignForce;
+
+
+    Vector2 velocity;
+    Vector2 acceleration;
+
+    List<GameObject> others = new List<GameObject>();
+
+
+
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+
+        // First we just translate based on our velocity
+        transform.Translate(velocity * Time.fixedDeltaTime);
+
+        // Add our acceleration
+        velocity += acceleration * Time.fixedDeltaTime;
+        if (velocity.magnitude > maxVelocity)
+        {
+            velocity = velocity.normalized * maxVelocity;
+        }
+
+
+        // Now we calculate the force towards the center of the flock
+        Vector2 cohesion;
+        Vector2 direction = FlockControl.center - (Vector2)transform.position;
+        float proximity = direction.magnitude;
+        cohesion = direction.normalized * cohesionForce;
+
+        // Next we calculate the repulsion force (if any)
+        Vector2 repulsion = Vector2.zero;
+        foreach(GameObject other in others)
+        {
+            // The force is away from the other, and increases when closer to this
+            direction = (Vector2)other.transform.position - (Vector2)transform.position;
+            proximity = (GetComponent<CircleCollider2D>().radius - direction.magnitude) / GetComponent<CircleCollider2D>().radius;
+            repulsion += direction.normalized * proximity * separationForce;
+        }
+
+        //Finally we get the direction we should be heading based on the flock and the mouse position
+        Vector2 align = FlockControl.target.normalized * alignForce;
+
+        acceleration = cohesion + repulsion + align;
+        if (acceleration.magnitude > maxAcceleration)
+        {
+            acceleration = acceleration.normalized * maxAcceleration;
+        }
+        velocity += acceleration * Time.fixedDeltaTime;
 	}
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        others.Add(collision.gameObject);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        others.Remove(collision.gameObject);
+    }
 }
