431fe48a47e645269c231741d29c7ec0b9426ac7
figgyfunk <figa@rpi.edu>
2018-10-09 16:24:53 -0400

    seek implemented.

---
+++ Multi-Agent Movement/Assets/Scripts/Character.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    public Kinematic staticInfo;
+    public SteeringOutput steering;
+    public float r;
+    public float a;
+    public Vector2 l; 
+
+    public struct SteeringOutput
+    {
+        public float angular;
+        public Vector3 linear;
+        public SteeringOutput(float _angular, Vector3 _linear)
+        {
+            this.angular = _angular;
+            this.linear = _linear;
+        }
+    }
+    public struct Kinematic
+    {
+        public Vector3 position;
+        public float orientation;
+        public Vector3 velocity;
+        public float rotation;
+        
+        public Kinematic(Vector3 _position, float _orientation, Vector3 _velocity, float _rotation)
+        {
+            this.position = _position;
+            this.orientation = _orientation;
+            this.velocity = _velocity;
+            this.rotation = _rotation;
+        }
+    }
+	// Use this for initialization
+	void Start ()
+    {
+        staticInfo = new Kinematic(transform.position, 0f, new Vector3(0, 0, 0), r);
+        steering = new SteeringOutput(a, l);
+	}
+	
+    public void updateSteering(float time)
+    {
+        staticInfo.position += staticInfo.velocity * time;
+        staticInfo.orientation += staticInfo.rotation * time;
+
+        staticInfo.velocity += steering.linear * time;
+        staticInfo.orientation += steering.angular * time;
+    }
+
+}
---
+++ Multi-Agent Movement/Assets/Scripts/CharacterManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CharacterManager : MonoBehaviour {
+
+    private Character character;
+    private Kinematics kinematics;
+    public GameObject target;
+    public float rotation;
+    public float maxSpeed;
+    public float angular;
+    public Vector2 linear;
+
+    private void Start()
+    {
+        kinematics = this.GetComponent<Kinematics>();
+        kinematics.character = this.gameObject;
+        kinematics.maxSpeed = maxSpeed;
+        kinematics.target = target.transform.position;
+
+        character = this.GetComponent<Character>();
+        character.r = rotation;
+        character.a = angular;
+        character.l = linear;
+    }
+    // Update is called once per frame
+    void Update ()
+    {
+        Kinematics.KinematicSteeringOutput steering = kinematics.Seek();
+
+        character.staticInfo.velocity = steering.velocity;
+        character.updateSteering(Time.deltaTime);
+
+        this.transform.position = character.staticInfo.position;
+        this.transform.rotation = Quaternion.Euler(0, 0, character.staticInfo.orientation * Mathf.Rad2Deg);
+    }
+}
