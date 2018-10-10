7a0155c7c4d5fb05bb09ac7e960c7c45754ad5fd
figgyfunk <figa@rpi.edu>
2018-10-09 21:28:04 -0400

    collision avoidance working first time.

--- Multi-Agent Movement/Assets/Scripts/CharacterManager.cs
+++ Multi-Agent Movement/Assets/Scripts/CharacterManager.cs
@@ -7,14 +7,20 @@ public class CharacterManager : MonoBehaviour {
 
     private Character character;
     private Kinematics kinematics;
+    private Collision collision;
     public GameObject target;
     public float rotation;
     public float maxSpeed;
     public float angular;
     public Vector2 linear;
+    public float lookAhead;
+    public float avoidDistance;
+
+    private GameObject debug;
 
     private void Start()
     {
+        debug = Instantiate(target);
         kinematics = this.GetComponent<Kinematics>();
         kinematics.character = this.gameObject;
         kinematics.maxSpeed = maxSpeed;
@@ -24,10 +30,21 @@ public class CharacterManager : MonoBehaviour {
         character.r = rotation;
         character.a = angular;
         character.l = linear;
+
+        collision = this.GetComponent<Collision>();
+        collision.character = character;
+        collision.lookAhead = lookAhead;
+        collision.avoidDistance = avoidDistance;
+        collision.whiskerLookAhead = lookAhead / 2;
     }
     // Update is called once per frame
     void Update ()
     {
+
+        Vector3 newTarget = collision.collisionTarget(kinematics.target);
+        
+        debug.transform.position = newTarget;
+        kinematics.target = newTarget;
         Kinematics.KinematicSteeringOutput steering = kinematics.Seek();
 
         character.staticInfo.velocity = steering.velocity;
@@ -35,5 +52,8 @@ public class CharacterManager : MonoBehaviour {
 
         this.transform.position = character.staticInfo.position;
         this.transform.rotation = Quaternion.Euler(0, 0, character.staticInfo.orientation * Mathf.Rad2Deg);
+
+        if((character.staticInfo.position - kinematics.target).magnitude < .1) { kinematics.target = target.transform.position; }
+        
     }
 }
--- Multi-Agent Movement/Assets/Scripts/Collision.cs
+++ Multi-Agent Movement/Assets/Scripts/Collision.cs
@@ -4,13 +4,98 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float avoidDistance;
+    public float lookAhead;
+    public float whiskerLookAhead;
+    public Character character;
+
+
+    public struct CollisionData
+    {
+        public Vector3 position;
+        public Vector3 normal;
+
+        public CollisionData(Vector3 _position, Vector3 _normal)
+        {
+            this.position = _position;
+            this.normal = _normal;
+        }
+    }
+
+    public CollisionData getCollision(Vector3 position, Vector3 moveAmount, Vector3 currentTarget, float _lookAhead)
+    {
+        
+        RaycastHit2D hit;
+        Debug.DrawRay(position, moveAmount);
+        if(hit = Physics2D.Raycast(position, moveAmount))
+        {
+            if(hit.transform != this.transform)
+            {
+
+            }
+            if(hit.distance <= _lookAhead)
+            {
+                CollisionData returnData = new CollisionData(hit.transform.position, hit.normal);
+                return returnData;
+            }
+        }
+        return new CollisionData(currentTarget, new Vector3(0,0));
+    }
+	public Vector3 collisionTarget(Vector3 currentTarget)
+    {
+        Vector3 rayVector = character.staticInfo.velocity;
+        rayVector.Normalize();
+        rayVector *= lookAhead;
+
+        Vector3 whisker_1 = Quaternion.AngleAxis(30, Vector3.up) * character.staticInfo.velocity ;
+        whisker_1.Normalize();
+        whisker_1 *= whiskerLookAhead;
+        Vector3 whisker_2 = Quaternion.AngleAxis(330, Vector3.up) * character.staticInfo.velocity;
+        whisker_2.Normalize();
+        whisker_2 *= whiskerLookAhead;
+
+        //Debug.DrawRay(character.staticInfo.position, whisker_1);
+        //Debug.DrawRay(character.staticInfo.position, whisker_2);
+        Debug.DrawRay(character.staticInfo.position, rayVector);
+        Vector2 newTarget = currentTarget;
+        RaycastHit2D hit; 
+        if(hit = Physics2D.Raycast(character.staticInfo.position, rayVector))
+        {
+            if(hit.transform != this.transform)
+            {
+                if(hit.distance <= lookAhead)
+                {
+                    newTarget = hit.point + hit.normal * avoidDistance;
+                }
+                
+            }
+        }
+      
+
+        if(hit = Physics2D.Raycast(character.staticInfo.position, whisker_1))
+        {
+            if(hit.transform != this.transform)
+            {
+                if(hit.distance <= whiskerLookAhead)
+                {
+                    newTarget += hit.point + hit.normal * avoidDistance;
+                }
+                
+            }
+        }
+
+        if (hit = Physics2D.Raycast(character.staticInfo.position, whisker_2))
+        {
+            if (hit.transform != this.transform)
+            {
+                if (hit.distance <= whiskerLookAhead)
+                {
+                    newTarget += hit.point + hit.normal * avoidDistance;
+                }
+            }
+        }
+
+
+        return newTarget;
+    }
 }
