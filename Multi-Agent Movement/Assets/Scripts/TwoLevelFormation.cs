8d08445d8bac26c1744f13858a795561bf271f94
CJ Legato <legatc@rpi.edu>
2018-10-28 18:33:25 -0400

    Ok I've tried and i dont know anymore im sorry

---
+++ Multi-Agent Movement/Assets/Scripts/TwoLevelFormation.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoLevelFormation : MonoBehaviour {
+
+    public int slot;
+    public Transform target;
+    public Vector2 offset = Vector2.zero;
+
+    [HideInInspector]
+    public Vector2 velocity = Vector2.zero;
+    Vector2 acceleration = Vector2.zero;
+
+    public LayerMask collisionlayer;
+
+    // Update is called once per frame. Using late update so that this updates after the manager
+    void LateUpdate () {
+
+        Arrive();
+        velocity += acceleration * Time.deltaTime;
+        if (velocity.magnitude > 3.5f)
+        {
+            velocity = velocity.normalized * 3.5f;
+        }
+        transform.position += (Vector3)velocity * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "ControlledBoid")
+        {
+            print("hi");
+            target.transform.parent.GetComponent<TwoLevelManager>().Remove(this.gameObject);
+        }
+    }
+
+    public void Arrive()
+    {
+        /* Get the right direction for the linear acceleration */
+        Vector2 targetPos = (Vector2)target.position + offset * target.forward;
+        Vector2 targetVelocity = targetPos - (Vector2)transform.position;
+
+        // Now we move our target based on collision detection
+        // First determine where to raycast
+        Vector3 rayVector = targetVelocity;
+        rayVector.Normalize();
+        Vector3 whisker_1 = Quaternion.AngleAxis(30, Vector3.up) * rayVector;
+        whisker_1.Normalize();
+        Vector3 whisker_2 = Quaternion.AngleAxis(330, Vector3.up) * rayVector;
+        whisker_2.Normalize();
+
+        // Now check ahead of us, and move the target if something is in the way
+        RaycastHit2D hit;
+        if (hit = Physics2D.Raycast(transform.position, rayVector, .5f, collisionlayer))
+        {
+            targetPos = targetPos + (hit.point - hit.normal * .25f) / 2f;
+            targetVelocity = targetPos - (Vector2)transform.position;
+        }
+
+        else if (hit = Physics2D.Raycast(transform.position, whisker_1, .25f, collisionlayer))
+        {
+            targetPos = targetPos + (hit.point - hit.normal * 0.125f) / 2f;
+            targetVelocity = targetPos - (Vector2)transform.position;
+        }
+
+        else if (hit = Physics2D.Raycast(transform.position, whisker_2, .25f, collisionlayer))
+        {
+            targetPos = targetPos + (hit.point - hit.normal * 0.125f) / 2f;
+            targetVelocity = targetPos - (Vector2)transform.position;
+        }
+
+        /* Get the distance to the target */
+        float dist = targetVelocity.magnitude;
+
+        /* If we are within the stopping radius then stop */
+        if (dist < .005)
+        {
+            velocity = Vector2.zero;
+            acceleration = Vector2.zero;
+        }
+
+        /* Calculate the target speed, full speed at slowRadius distance and 0 speed at 0 distance */
+        float targetSpeed;
+        if (dist > 1)
+        {
+            targetSpeed = 3.5f;
+        }
+        else
+        {
+            targetSpeed = 3.5f * (dist / 3.5f);
+        }
+
+        /* Give targetVelocity the correct speed */
+        targetVelocity.Normalize();
+        targetVelocity *= targetSpeed;
+
+        /* Calculate the linear acceleration we want */
+        acceleration = targetVelocity - velocity;
+        acceleration *= 4f;
+
+        /* Make sure we are accelerating at max acceleration */
+        if (acceleration.magnitude > 10f)
+        {
+            acceleration.Normalize();
+            acceleration *= 10f;
+        }
+
+    }
+}
---
+++ Multi-Agent Movement/Assets/Scripts/TwoLevelManager.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoLevelManager : MonoBehaviour {
+
+    public GameObject boid;
+    public int numBoids;
+    public Vector2 offset = new Vector2(-0.5f, 0.2f);
+
+    public Transform[] path;
+    int index = 0;
+
+    List<GameObject> Boids;
+    List<GameObject> targets;
+
+    Dictionary<int, GameObject> boidToTarget;
+
+	// Use this for initialization
+	void Start () {
+
+        transform.position = path[0].position;
+        index = 1;
+        Vector2 direction = (Vector2)path[1].position - (Vector2)transform.position;
+        direction.Normalize();
+        float rot_z = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z);
+
+
+        GameObject targetSpawn = new GameObject("Target");
+        targets = new List<GameObject>();
+        Boids = new List<GameObject>();
+        boidToTarget = new Dictionary<int, GameObject>();
+        for (int i = 0; i < numBoids; i++) {
+            Vector2 tempOffset = offset * ((i / 2) + 1);
+            if (i % 2 == 1)
+            {
+                tempOffset.y = tempOffset.y * -1;
+            }
+            GameObject target = Instantiate(targetSpawn, transform, false);
+            target.transform.localPosition = tempOffset;
+            targets.Add(target);
+
+            GameObject temp = Instantiate(boid, target.transform.position, Quaternion.identity);
+            temp.GetComponent<TwoLevelFormation>().slot = i;
+            temp.GetComponent<TwoLevelFormation>().target = target.transform;
+            Boids.Add(temp);
+            boidToTarget.Add(i, target);
+        }
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 anchorPos = Vector2.zero;
+        Vector2 anchorVel = Vector2.zero;
+
+        int i = 0;
+        foreach (GameObject boid in Boids)
+        {
+            anchorVel += boid.GetComponent<TwoLevelFormation>().velocity;
+            Vector2 localOffset = (Vector2)boid.GetComponent<TwoLevelFormation>().target.position - (Vector2)boid.transform.position;
+            anchorPos += localOffset;
+            i += 1;
+        }
+
+        anchorPos /= (float)i;
+        anchorVel /= (float)i;
+        //transform.position -= (Vector3)anchorPos;
+
+        Vector2 target = path[index].position;
+        if (index < path.Length - 1 && Vector2.Distance(path[index].position, transform.position) < 0.3f)
+        {
+            if (anchorPos.magnitude < 0.3f)
+            {
+                index += 1;
+                target = path[index].position;
+            } else
+            {
+                target = (Vector2)(transform.position);
+            }
+        }
+
+        
+        Vector2 direction = target - (Vector2)transform.position;
+        if (direction != Vector2.zero)
+        {
+            direction.Normalize();
+            float rot_z = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, rot_z);
+            transform.position += (Vector3)direction * 1.5f * Time.deltaTime;
+        }
+    }
+
+    public void Remove(GameObject boid)
+    {
+        if (!Boids.Contains(boid)) { return; }
+
+        int slot = boid.GetComponent<TwoLevelFormation>().slot;
+        Transform target = boid.GetComponent<TwoLevelFormation>().target;
+        Boids.Remove(boid);
+        Destroy(boid);
+        for (int i = 0; i < Boids.Count; i++)
+        {
+            if (Boids[i].GetComponent<TwoLevelFormation>().slot > slot)
+            {
+                int temp = Boids[i].GetComponent<TwoLevelFormation>().slot;
+                Boids[i].GetComponent<TwoLevelFormation>().slot = slot;
+                slot = temp;
+                Transform tempTarget = Boids[i].GetComponent<TwoLevelFormation>().target;
+                Boids[i].GetComponent<TwoLevelFormation>().target = target;
+                target = tempTarget;
+            }
+        }
+    }
+}
